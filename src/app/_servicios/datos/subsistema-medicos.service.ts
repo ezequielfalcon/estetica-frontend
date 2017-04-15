049f70f48b52e4fb4794b9da0c245fe2eb5a2c83
Ezequiel Falcón <falconezequiel@gmail.com>
2017-04-15 19:42:13 -0300

    servicio de subsistema de medicos listo

--- src/app/_servicios/datos/subsistema-medicos.service.ts
+++ src/app/_servicios/datos/subsistema-medicos.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpAuthService} from "../http-auth.service";
+import {Response} from "@angular/http";
 
 @Injectable()
 export class SubsistemaMedicosService {
 
-  constructor() { }
+  constructor(private http: HttpAuthService) { }
+
+  verTurnos(fecha: string){
+    return this.http.get('/sub-medicos/turnos/' + fecha).map((response: Response) => response.json().datos);
+  }
+
+  turnoAtendido(turnoId: number){
+    let body = new URLSearchParams();
+    return this.http.put('/sub-medicos/turnos/' + turnoId, body).map((response: Response) => response.json());
+  }
 
 }
