b7e61f9e2a16d25649de9be3f13df1669b809fd4
Ezequiel Falcón <ezequiel.falcon@unitecblue.com>
2017-04-19 16:56:34 -0300

    bajados los required de campos para que sólo sea obligatorio nombre, apellido y dni. sólo en componente nuevo-paciente. falta para ver-modificar para #6

--- src/app/_componentes/pacientes/nuevo-paciente/nuevo-paciente.component.ts
+++ src/app/_componentes/pacientes/nuevo-paciente/nuevo-paciente.component.ts
@@ -55,11 +55,10 @@ export class NuevoPacienteComponent implements OnInit, OnDestroy {
   }
 
   crear(){
-    if (!this.nuevoPac.obvservaciones) this.nuevoPac.obvservaciones = " ";
     this.pacientesService.post(this.nuevoPac.nombre, this.nuevoPac.apellido,
-      this.nuevoPac.documento, this.nuevoPac.tel, this.nuevoPac.email,
-      this.nuevoPac.fechaNac, this.nuevoPac.sexo, this.nuevoPac.id_os,
-      this.nuevoPac.numero_os, this.nuevoPac.domicilio, this.nuevoPac.obvservaciones )
+      this.nuevoPac.documento, this.nuevoPac.tel || ' ', this.nuevoPac.email || ' ',
+      this.nuevoPac.fechaNac || '1901-01-01', this.nuevoPac.sexo || 'N', this.nuevoPac.id_os || 7359,
+      this.nuevoPac.numero_os || ' ', this.nuevoPac.domicilio || ' ', this.nuevoPac.obvservaciones || ' ')
       .subscribe(nuevoP => {
         this.notif.success('OK', 'Paciente creado con ID ' + nuevoP.id);
         this.router.navigate([this.returnUrl]);
