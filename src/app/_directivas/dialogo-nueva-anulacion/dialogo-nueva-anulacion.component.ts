ad742b231ea785dc17817cd0ae363d6ea711f196
Ezequiel Falcón <falconezequiel@gmail.com>
2017-04-22 15:57:43 -0300

    nuevo componente para crear anulaciones

---
+++ src/app/_directivas/dialogo-nueva-anulacion/dialogo-nueva-anulacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialogo-nueva-anulacion',
+  templateUrl: './dialogo-nueva-anulacion.component.html',
+  styleUrls: ['./dialogo-nueva-anulacion.component.css']
+})
+export class DialogoNuevaAnulacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
