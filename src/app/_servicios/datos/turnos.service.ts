e3980530403bdb6abda47279e5843ea9a10e54be
Ezequiel Falcón <falconezequiel@gmail.com>
2017-05-05 01:02:05 -0300

    servicio de turnos por id #83

--- src/app/_servicios/datos/turnos.service.ts
+++ src/app/_servicios/datos/turnos.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {HttpAuthService} from "../http-auth.service";
-import {Response, URLSearchParams} from "@angular/http";
-import {Observable} from "rxjs";
+import {HttpAuthService} from '../http-auth.service';
+import {Response, URLSearchParams} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class TurnosService {
@@ -10,74 +10,65 @@ export class TurnosService {
     private http: HttpAuthService
   ) { }
 
-
-
-  traerConfig(){
-    return this.http.get('/configuracion-turnos').map((response: Response) => response.json().datos);
-  }
-
-  traerTurnos(fecha: string, pararPolling){
-    return Observable.interval(10000).flatMap(() => {
-      return this.http.get('/turnos/' + fecha).map((response: Response) => response.json().datos)
-        .takeUntil(pararPolling);
-    });
-  }
-
-  traerTurnosResumido(fecha: string, pararPolling){
+  traerTurnosResumido(fecha: string, pararPolling) {
     return Observable.interval(5000).flatMap(() => {
       return this.http.get('/agenda/' + fecha).map((response: Response) => response.json().datos)
         .takeUntil(pararPolling);
     });
   }
 
-  traerTurno(consultorio, turno: number, fecha: string, entreturno: boolean){
+  traerTurno(consultorio, turno: number, fecha: string, entreturno: boolean) {
     return this.http.get('/turnos/' + fecha
       + '/' + consultorio + '/' + turno + '/' + entreturno).map((response: Response) => response.json().datos);
   }
 
-  traerTurnosPorMedico(medicoId: number, fecha: string){
+  traerTurnoPorId(turnoId: number) {
+    return this.http.get('/turno/' + turnoId).map((response: Response) => response.json().datos);
+  }
+
+  traerTurnosPorMedico(medicoId: number, fecha: string) {
     return this.http.get('/turnos/' + fecha + '/' + medicoId).map((response: Response) => response.json().datos);
   }
 
-  nuevoTurno(turnoId:number , pacienteId:number, consultorioId:number, medicoId:number,
-             obs, costo, fecha:string, entreturno: boolean){
-    let body = new URLSearchParams();
-    body.set('id_turno', ""+turnoId);
-    body.set('id_paciente', ""+pacienteId);
-    body.set('id_consultorio', ""+consultorioId);
-    body.set('id_medico', ""+medicoId);
+  nuevoTurno(turnoId: number , pacienteId: number, consultorioId: number, medicoId: number,
+             obs, costo, fecha: string, entreturno: boolean) {
+    const body = new URLSearchParams();
+    body.set('id_turno', '' + turnoId);
+    body.set('id_paciente', '' + pacienteId);
+    body.set('id_consultorio', '' + consultorioId);
+    body.set('id_medico', '' + medicoId);
     body.set('observaciones', obs);
     body.set('costo', costo);
     body.set('fecha', fecha);
-    body.set('entreturno', ""+entreturno);
+    body.set('entreturno', '' + entreturno);
     return this.http.post('/nuevo-turno', body).map((response: Response) => response.json().id);
   }
 
-  agregarTratamiento(agendaId: number, tratamientoId: number){
-    let body = new URLSearchParams();
-    body.set('id_agenda', ""+agendaId);
-    body.set('id_tratamiento', ""+tratamientoId);
+  agregarTratamiento(agendaId: number, tratamientoId: number) {
+    const body = new URLSearchParams();
+    body.set('id_agenda', '' + agendaId);
+    body.set('id_tratamiento', '' + tratamientoId);
     return this.http.post('/agregar-tratamiento', body).map((response: Response) => response.json());
   }
 
-  confirmarPresencia(agendaId: number, presencia: boolean){
-    let body = new URLSearchParams();
-    body.set("id_agenda", ""+agendaId);
-    body.set("presente", ""+presencia);
+  confirmarPresencia(agendaId: number, presencia: boolean) {
+    const body = new URLSearchParams();
+    body.set('id_agenda', '' + agendaId);
+    body.set('presente', '' + presencia);
     return this.http.post('/turno-presente', body).map((response: Response) => response.json());
   }
 
-  borrar(turnoId: number){
+  borrar(turnoId: number) {
     return this.http.del('/turnos/' + turnoId).map((response: Response) => response.json());
   }
 
-  verHorarios(){
+  verHorarios() {
     return this.http.get('/horarios').map((response: Response) => response.json().datos);
   }
 
   modificarCosto(turnoId: number, costo: number) {
-    let body = new URLSearchParams();
-    body.set("costo", ""+costo);
+    const body = new URLSearchParams();
+    body.set('costo', '' + costo);
     return this.http.put('/agenda/modificar-costo/' + turnoId, body).map((response: Response) => response.json());
   }
 
