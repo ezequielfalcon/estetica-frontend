dd91b3158c06ba73c2cb284b0ecf55c9ff39d3ff
Ezequiel Falcón <falconezequiel@gmail.com>
2017-05-05 00:47:36 -0300

    fix en costo de turno para #83

--- src/app/_directivas/dialogo-modificar-costo-turno/dialogo-modificar-costo-turno.component.ts
+++ src/app/_directivas/dialogo-modificar-costo-turno/dialogo-modificar-costo-turno.component.ts
@@ -1,9 +1,9 @@
-import {Component, OnInit, ViewContainerRef} from '@angular/core';
-import {SpinnerService} from "../../_servicios/spinner.service";
-import {NotificationsService} from "angular2-notifications";
-import {Router} from "@angular/router";
-import {MdDialogRef} from "@angular/material";
-import {TurnosService} from "../../_servicios/datos/turnos.service";
+import {Component, OnInit} from '@angular/core';
+import {SpinnerService} from '../../_servicios/spinner.service';
+import {NotificationsService} from 'angular2-notifications';
+import {Router} from '@angular/router';
+import {MdDialogRef} from '@angular/material';
+import {TurnosService} from '../../_servicios/datos/turnos.service';
 
 @Component({
   selector: 'app-dialogo-modificar-costo-turno',
@@ -23,26 +23,26 @@ export class DialogoModificarCostoTurnoComponent implements OnInit {
     private router: Router,
     public dialogRef: MdDialogRef<DialogoModificarCostoTurnoComponent>,
     private turnosService: TurnosService
-  ) { }
+  ) {
+    this.nuevoCosto = 0;
+  }
 
   ngOnInit() {
   }
 
   modificar() {
-    if (this.nuevoCosto) {
-      this.spinner.start();
-      this.turnosService.modificarCosto(this.agendaId, this.nuevoCosto).subscribe(() => {
-        this.dialogRef.close();
-        this.spinner.stop();
-      }, error => {
-        if (error.status === 401) {
-          this.notificationService.error('Error', 'Sesión expirada!');
-          this.router.navigate(['/login']);
-        }
-        const body = JSON.parse(error._body);
-        this.notificationService.error('Error', body.mensaje);
-        this.spinner.stop();
-      });
-    }
+    this.spinner.start();
+    this.turnosService.modificarCosto(this.agendaId, this.nuevoCosto).subscribe(() => {
+      this.dialogRef.close();
+      this.spinner.stop();
+    }, error => {
+      if (error.status === 401) {
+        this.notificationService.error('Error', 'Sesión expirada!');
+        this.router.navigate(['/login']);
+      }
+      const body = JSON.parse(error._body);
+      this.notificationService.error('Error', body.mensaje);
+      this.spinner.stop();
+    });
   }
 }
