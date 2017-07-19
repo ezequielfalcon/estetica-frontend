8c8c8c75853e03ddd22b5759e156ff293d8af25c
Ezequiel Falcón <falconezequiel@gmail.com>
2017-07-19 12:35:55 -0300

    Revert "Ficha paciente"

--- src/app/_servicios/dialogos/dialogo-turno.service.ts
+++ src/app/_servicios/dialogos/dialogo-turno.service.ts
@@ -20,13 +20,4 @@ export class DialogoTurnoService {
     return dialogRef.afterClosed();
   }
 
-  verTurnoId(agendaId: number, viewContainerRef: ViewContainerRef) {
-    let dialogRef: MdDialogRef<DialogoTurnoComponent>;
-    let config = new MdDialogConfig();
-    config.viewContainerRef = viewContainerRef;
-    dialogRef = this.dialog.open(DialogoTurnoComponent, config);
-    dialogRef.componentInstance.agendaId = agendaId;
-    return dialogRef.afterClosed();
-  }
-
 }
