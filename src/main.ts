186bba48191dc4627f016441dea06f1138a70ffd
Ezequiel Falcón <falconezequiel@gmail.com>
2017-07-27 15:12:30 -0300

    fixes menores de compilación

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import {DarkThemeService} from "./app/_servicios/dark-theme.service";
 
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic()
-  .bootstrapModule(AppModule, [DarkThemeService])
-  .catch(err => console.error(err));
+  .bootstrapModule(AppModule)
+  .catch(err => console.log(err));
